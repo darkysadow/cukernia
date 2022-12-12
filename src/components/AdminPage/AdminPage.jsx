ea8fbc0761b7f33be8b932671e76c6ca0aff3a12
darkysadow <alexsmithshow@gmail.com>
2022-12-12 20:37:46 +0200

    Created 2 main blocks in AdminPage

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { Navigate } from "react-router";
+import { useAuth } from "../../utilites/firebase/auth";
 import s from './AdminPage.module.css';
 
 const AdminPage = (props) => {
+    const { authUser } = useAuth()
     return (
-        <div className={`${s.adminPage} ${"container"}`}>
-            admin panel йбадь його нахуй блять
-        </div>
+        !authUser ?
+            <Navigate to={'/login'} />
+            :
+            <div className={`${s.adminPage} ${"container"}`}>
+                <div className={s.categories}>
+                    <div className={s.topicName}>
+                        категорії
+                    </div>
+                </div>
+                <div className={s.dishes}>
+                    <div className={s.topicName}>
+                        страви
+                    </div>
+                </div>
+            </div>
     );
 }
 
