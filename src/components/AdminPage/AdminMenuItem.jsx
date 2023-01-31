29b12bdc5eb8382d60aedad085d7ca1cb1baf59b
darkysadow <alexsmithshow@gmail.com>
2023-01-31 18:20:28 +0200

    Refactored menu on admin page, refactored menu items, added fontAwesome to update&delete buttons

--- src/components/AdminPage/AdminMenuItem.jsx
+++ src/components/AdminPage/AdminMenuItem.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 import s from './AdminMenuItem.module.css';
+import { faTrashCan, faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AdminMenuItem = (props) => {
     const dish = props.dish;
     return (<div className={s.adminMenuItem}>
-        <div className={s.leftHalf}>
-            <div className={s.menuItemImage}>
-                <img src={dish.imageURL} alt="" />
+        {!dish.available && <div className={s.unavailable}></div>}
+        <div className={s.menuItemBlur}></div>
+        <div className={s.menuItemImage}>
+            <img src={dish.imageURL} alt="" />
+        </div>
+        <div className={s.menuItemNameDescription}>
+            <div className={s.menuItemName}>
+                {dish.dishName}
             </div>
-            <div className={s.menuItemNameDescription}>
-                <div className={s.menuItemName}>
-                    <p>Назва:</p>
-                    {dish.dishName}
-                </div>
-                <div className={s.menuItemDescription}>
-                    <p>Опис:</p>
-                    {dish.description}
-                </div>
+            <div className={s.menuItemDescription}>
+                {dish.description}
             </div>
         </div>
-        <div className={s.rightHalf}>
         <div className={s.menuItemPricePortion}>
             <div className={s.menuItemPrice}>
-                Ціна: {dish.price} ₴
+                {dish.price} ₴
             </div>
             <div className={s.menuItemPortion}>
-                Порція: {dish.portion}
+                {dish.portion}
             </div>
         </div>
         <div className={s.menuItemAvailable}>
             {dish.available ? 'Доступно' : 'Недоступно'}
         </div>
         <div className={s.menuItemControls}>
-            <div className={s.menuItemUpdate}>U</div>
-            <div className={s.menuItemDelete}>D</div>
-        </div>
+            <div className={s.menuItemUpdate}>
+                <FontAwesomeIcon icon={faPenToSquare} style={{color: '#3784ff'}}/>
+            </div>
+            <div className={s.menuItemDelete}>
+                <FontAwesomeIcon icon={faTrashCan} style={{color: '#ff3737'}}/>
+            </div>
         </div>
     </div>)
 }
