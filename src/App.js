3663aca3c475f0b47114b30b5c9297faac34c397
darkysadow <alexsmithshow@gmail.com>
2023-02-05 15:40:11 +0200

    Deleted: -redux, -api; Because they are not used in the project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Body from './components/Body';
 import Header from './components/Header/Header';
-import store from './redux/redux-store';
 import { AuthUserProvider } from './utilites/firebase/auth';
 
 function App() {
   return (
     <AuthUserProvider>
-    <Provider store={store}>
       <BrowserRouter>
         <div className='wrapper'>
           <Header />
@@ -17,7 +14,6 @@ function App() {
           <footer></footer>
         </div>
       </BrowserRouter>
-    </Provider>
     </AuthUserProvider>
   );
 }
--- src/api/api.js
+++
@@ -1,7 +0,0 @@
-//import axios from "axios";
-
-export const menuAPI = {
-    getMenu(category) {
-        //return axios.get(`https://misteram.com.ua/api/company-category-items?city=vinnytsia&category=${category}&company=tsukernia`);
-    }
-}
